f350873e10a4a63af6f15ffc6c9fe49e0158605f
mostafamagde <mostafamagde228@gmail.com>
2024-10-29 03:12:17 +0300

    add gemini package

--- lib/api_manager/api_manager.dart
+++ lib/api_manager/api_manager.dart
@@ -1,64 +1,15 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
-
-import '../models/gemi_model.dart';
-import 'package:http/http.dart' as http;
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 class ApiManager {
-  static Future<GemiModel> getGemiResponse(String req) async {
-    var headers = {'Content-Type': 'application/json'};
-    var data = json.encode({
-      "contents": [
-        {
-          "parts": [
-            {"text": req}
-          ]
-        }
-      ]
-    });
-    var dio = Dio();
-    var response = await dio.request(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyB9GWJSEZwbAs1Lby-0azbq0jHmacj2J2Q',
-      options: Options(
-        method: 'POST',
-        headers: headers,
-      ),
-      data: data,
+  static Future<String> gemiRequest(String req) async {
+    final model = GenerativeModel(
+      model: 'gemini-1.5-flash',
+      apiKey: "AIzaSyB9GWJSEZwbAs1Lby-0azbq0jHmacj2J2Q",
     );
+    final prompt = req;
 
-    if (response.statusCode == 200) {
-      return GemiModel.fromJson(response.data);
-    } else {
-      throw Exception(response.statusCode);
-    }
-  }
+    final response = await model.generateContent([Content.text(prompt)]);
 
-  static Future<GemiModel> test(String req) async {
-    var url = Uri.https(
-        "generativelanguage.googleapis.com",
-        "/v1beta/models/gemini-1.5-flash-latest:generateContent",
-        {"key": "AIzaSyB9GWJSEZwbAs1Lby-0azbq0jHmacj2J2Q"});
-    final response = await http.post(
-      url,
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode(
-        {
-          "contents": [
-            {
-              "parts": [
-                {"text": req}
-              ]
-            }
-          ]
-        },
-      ),
-    );
-    if (response.statusCode == 200) {
-      return GemiModel.fromJson(jsonDecode(response.body));
-
-    }else {
-      throw Exception("Failed to get response: ${response.statusCode}");
-    }
+    return response.text!;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,22 @@
-
 import 'package:flutter/material.dart';
 
-import 'api_manager/api_manager.dart';
 import 'core/routes_manager/route_generator.dart';
 import 'core/routes_manager/routes_names.dart';
 
 void main() {
   runApp(const MyApp());
-ApiManager.test("what do you know about egypt?");
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute:RoutesNames.splashView,
+      initialRoute: RoutesNames.splashView,
       onGenerateRoute: RouteGenerator.generateRoutes,
       title: 'Ai interview prtotype',
-
-
     );
   }
 }
